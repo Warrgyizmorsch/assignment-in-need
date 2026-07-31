421dbbacc1fce991b788c26fe21f3f0dbdf62548
wmranjit11-commits <wmranjit11@gmail.com>
2026-07-31 16:31:42 +0530

    changes done

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -99,9 +99,11 @@ export default function HeroSection() {
                   {[...Array(5)].map((_, i) => (
                     <svg
                       key={i}
+                      width="11"
+                      height="11"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      className="w-[11px] h-[11px] max-[480px]:w-3 max-[480px]:h-3 text-white"
+                      className="w-[11px] h-[11px] max-[480px]:w-3 max-[480px]:h-3 text-white shrink-0"
                     >
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
                     </svg>
